d62f8d644af82503ae7e5d872a8fbecdb5be910f
Ethan222 <ethannworks@gmail.com>
2024-03-27 18:08:57 -0400

    added card and deck classes

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 class Card {
+    val;
+    suit;
     constructor(val, suit) {
         this.val = val;
         this.suit = suit;
     }
     toString() {
-        return `${this.val} ${this.suit}`;
+        return `${this.val > 0 ? "+" : ""}${this.val} ${this.suit}`;
+    }
+    get imageSrc() {
+        return this.toString() + ".png";
     }
 }
-
-let testCard = new Card(1, "Triangle");
-document.getElementById("test").innerText = testCard;
+class Deck {
+    cards;
+    suits = ["Circle", "Square", "Triangle"];
+    constructor() {
+        this.reset();
+    }
+    reset() {
+        this.cards = [];
+        for(const suit of this.suits) {
+            for(let val = 1; val <= 10; val++) {
+                this.cards.push(new Card(val, suit), new Card(-val, suit));
+            }
+        }
+        let sylop = new Card(0, "sylop");
+        this.cards.push(sylop, sylop);
+    }
+    draw() {
+        return this.cards.splice(Math.floor(Math.random() * this.cards.length), 1);
+    }
+}
+const deck = new Deck();
+let card = deck.draw();
+document.getElementById("playArea").innerHTML = `<img src=${card.imageSrc} title=${card.toString()}>`;
